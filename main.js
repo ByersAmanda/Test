d1a45b9269a2f964247fe15127ef5f5b53a3f24a
ByersAmanda <50306555+ByersAmanda@users.noreply.github.com>
2019-10-23 23:11:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const date = document.getElementById('date');
+const time = document.getElementById('time');
+const message = document.getElementById('day-message');
+
+date.innerHTML = new Date().toLocaleDateString('en-US', {
+  year: '2-digit',
+  month: '2-digit',
+  day: '2-digit'
+});
+
+setInterval(() => {
+  time.innerHTML = new Date().toLocaleTimeString('en-US', {
+    hour12: true,
+    hour: 'numeric',
+    minute: 'numeric'
+  });
+}, 1000);
+
+const hour = new Date().getHours();
+
+function setMessage(hour) {
+  if (hour >= 12 && hour <= 17) {
+    message.innerHTML = 'Good afternoon.';
+  } else if (hour >= 18 && hour <= 23) {
+    message.innerHTML = 'Good night.';
+  } else {
+    message.innerHTML = 'Good morning.';
+  }
+}
+
+setInterval(setMessage(hour), 1000 * 60 * 60);
